3873cf395cebe8b177b014c98180388f4ceadef7
Chris Lorenzo <chiefcll@gmail.com>
2024-10-14 21:02:11 -0400

    Updates

---
+++ assets/Text-BQKUs0gW.js
@@ -0,0 +1,106 @@
+import { j as hexColor, g as onMount, h as createComponent, V as View, T as Text, s as setGlobalBackground } from "./index-D7DCTuhh.js";
+const TextPage = () => {
+  const OverviewContainer = {
+    width: 900,
+    height: 500,
+    y: 350,
+    x: 150,
+    gap: 25,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flexStart",
+    color: hexColor("00000000")
+  };
+  const SublineContainer = {
+    width: 900,
+    height: 36,
+    gap: 6,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flexStart",
+    color: hexColor("00000000")
+  };
+  const Title = {
+    fontSize: 42,
+    fontWeight: "bold"
+  };
+  const SubTitle = {
+    fontSize: 38,
+    fontWeight: 500
+  };
+  const Overview = {
+    width: OverviewContainer.width,
+    fontSize: 26,
+    fontWeight: "normal",
+    contain: "width"
+  };
+  const Subline = {
+    fontSize: 26,
+    fontWeight: 100
+  };
+  onMount(() => {
+    setGlobalBackground(255);
+  });
+  return [createComponent(View, {
+    autofocus: true,
+    style: OverviewContainer,
+    get children() {
+      return [createComponent(Text, {
+        style: Title,
+        children: "Title of the Page"
+      }), createComponent(Text, {
+        style: SubTitle,
+        children: "Tag line for the page"
+      }), createComponent(Text, {
+        style: Overview,
+        children: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel tempor tellus. Sed eu leo purus. Vestibulum sollicitudin eget tellus a varius. Phasellus est turpis, volutpat sed blandit sit amet, rutrum sit amet mauris. In dignissim elit orci, a sollicitudin ipsum faucibus et. Quisque vel quam rutrum, faucibus augue sed, scelerisque nunc."
+      }), createComponent(View, {
+        style: SublineContainer,
+        get children() {
+          return [createComponent(Text, {
+            style: Subline,
+            children: "Subline Text"
+          }), createComponent(View, {
+            width: 28,
+            height: 28,
+            src: "./assets/rt-popcorn.png"
+          }), createComponent(Text, {
+            style: Subline,
+            children: "More Text"
+          })];
+        }
+      })];
+    }
+  }), createComponent(View, {
+    width: 600,
+    display: "flex",
+    gap: 20,
+    height: 42,
+    y: 200,
+    x: 150,
+    get children() {
+      return [createComponent(Text, {
+        style: Title,
+        children: "Flex Grow"
+      }), createComponent(View, {
+        flexGrow: 1,
+        height: 4,
+        y: 19,
+        color: 4281336063
+      }), createComponent(View, {
+        flexGrow: 3,
+        height: 4,
+        y: 19,
+        color: 4281401343
+      }), createComponent(View, {
+        flexGrow: 1,
+        height: 4,
+        y: 19,
+        color: 4005178623
+      })];
+    }
+  })];
+};
+export {
+  TextPage as default
+};
