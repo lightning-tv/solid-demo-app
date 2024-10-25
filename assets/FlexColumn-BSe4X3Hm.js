cb5d5fa7142c904f031d0c93a15d9d3ced06f011
Chris Lorenzo <chiefcll@gmail.com>
2024-10-25 14:43:39 -0400

    Updates

---
+++ assets/FlexColumn-BSe4X3Hm.js
@@ -0,0 +1,121 @@
+import { j as hexColor, c as createSignal, g as onMount, h as createComponent, T as Text, R as Row, C as Column, V as View, m as mergeProps, s as setGlobalBackground } from "./index-Cn_bdWrH.js";
+const FlexColumnPage = () => {
+  const RowStyles = {
+    display: "flex",
+    justifyContent: "spaceEvenly",
+    width: 1920,
+    y: 100,
+    height: 880,
+    color: hexColor("00000000")
+  };
+  const ColumnStyles = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flexStart",
+    color: 1303115263,
+    height: 850,
+    width: 60
+  };
+  const rowTitle = {
+    fontSize: 44,
+    y: 20,
+    x: 150
+  };
+  function Block(props) {
+    const styles = {
+      width: 50,
+      height: 80,
+      x: 5,
+      color: 392801023
+    };
+    return createComponent(View, mergeProps(props, {
+      style: styles
+    }));
+  }
+  const [columnY, setColumnY] = createSignal(50);
+  function onFocus() {
+    this.children[this.selected || 0].setFocus();
+    setColumnY(150 + (this.y || 0) * -1);
+  }
+  onMount(() => {
+    setGlobalBackground(858993663);
+  });
+  const gap = 50;
+  return [createComponent(Text, {
+    style: rowTitle,
+    children: "Start, MarginTop, End, MarginBottom, Center, Between, Evenly"
+  }), createComponent(Row, {
+    gap,
+    style: RowStyles,
+    onFocus,
+    get children() {
+      return [createComponent(Column, {
+        gap: 30,
+        style: ColumnStyles,
+        get children() {
+          return [createComponent(Block, {
+            autofocus: true
+          }), createComponent(Block, {}), createComponent(Block, {}), createComponent(Block, {}), createComponent(Block, {})];
+        }
+      }), createComponent(Column, {
+        gap,
+        style: ColumnStyles,
+        onFocus,
+        get children() {
+          return [createComponent(Block, {
+            marginTop: 100
+          }), createComponent(Block, {}), createComponent(Block, {
+            marginTop: 100
+          }), createComponent(Block, {}), createComponent(Block, {})];
+        }
+      }), createComponent(Column, {
+        gap,
+        justifyContent: "flexEnd",
+        style: ColumnStyles,
+        onFocus,
+        get children() {
+          return [createComponent(Block, {}), createComponent(Block, {}), createComponent(Block, {}), createComponent(Block, {}), createComponent(Block, {})];
+        }
+      }), createComponent(Column, {
+        gap,
+        justifyContent: "flexEnd",
+        style: ColumnStyles,
+        onFocus,
+        get children() {
+          return [createComponent(Block, {}), createComponent(Block, {
+            marginBottom: 100
+          }), createComponent(Block, {}), createComponent(Block, {}), createComponent(Block, {
+            marginBottom: 100
+          })];
+        }
+      }), createComponent(Column, {
+        gap,
+        justifyContent: "center",
+        style: ColumnStyles,
+        onFocus,
+        get children() {
+          return [createComponent(Block, {}), createComponent(Block, {}), createComponent(Block, {}), createComponent(Block, {}), createComponent(Block, {})];
+        }
+      }), createComponent(Column, {
+        gap,
+        justifyContent: "spaceBetween",
+        style: ColumnStyles,
+        onFocus,
+        get children() {
+          return [createComponent(Block, {}), createComponent(Block, {}), createComponent(Block, {}), createComponent(Block, {}), createComponent(Block, {})];
+        }
+      }), createComponent(Column, {
+        gap,
+        justifyContent: "spaceEvenly",
+        style: ColumnStyles,
+        onFocus,
+        get children() {
+          return [createComponent(Block, {}), createComponent(Block, {}), createComponent(Block, {}), createComponent(Block, {}), createComponent(Block, {})];
+        }
+      })];
+    }
+  })];
+};
+export {
+  FlexColumnPage as default
+};
