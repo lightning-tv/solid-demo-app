532e910f56969b825925b0722db55dd12bf7f6d3
Chris Lorenzo <chiefcll@gmail.com>
2025-03-04 14:17:02 -0500

    Updates

---
+++ assets/FlexGrow-BCxzFyWT.js
@@ -0,0 +1,118 @@
+import { c as createSignal, g as onMount, h as createComponent, C as Column, A as styles, T as Text, R as Row, V as View, m as mergeProps, s as setGlobalBackground } from "./index-DPzisnhw.js";
+const FlexGrow = () => {
+  const RowStyles = {
+    display: "flex",
+    justifyContent: "flexStart",
+    width: 1600,
+    height: 110,
+    color: 4294967295
+  };
+  const rowTitle = {
+    fontSize: 44,
+    marginTop: 25,
+    marginBottom: -20,
+    skipFocus: true
+  };
+  const red = 4278190335;
+  const darkorange = 4287365375;
+  const green = 16711935;
+  function Block(props) {
+    const styles2 = {
+      width: props.flexGrow ? void 0 : 200,
+      // Allow flexible width if flexGrow exists
+      height: 100,
+      y: 5
+    };
+    return createComponent(View, mergeProps(props, {
+      style: styles2
+    }));
+  }
+  const [columnY, setColumnY] = createSignal(50);
+  function onFocus() {
+    this.children[this.selected || 0].setFocus();
+    setColumnY(150 + (this.y || 0) * -1);
+  }
+  onMount(() => {
+    setGlobalBackground(858993663);
+  });
+  const gap = 50;
+  return createComponent(Column, {
+    x: 160,
+    get y() {
+      return columnY();
+    },
+    gap: 30,
+    height: 850,
+    get width() {
+      return RowStyles.width;
+    },
+    get style() {
+      return styles.Column;
+    },
+    get children() {
+      return [createComponent(Text, {
+        style: rowTitle,
+        children: "Flex Start RTL"
+      }), createComponent(Row, {
+        gap,
+        direction: "rtl",
+        style: RowStyles,
+        onFocus,
+        get children() {
+          return [createComponent(Block, {
+            flexGrow: 1,
+            autofocus: true,
+            color: red
+          }), createComponent(Block, {
+            flexGrow: 2,
+            color: darkorange
+          }), createComponent(Block, {
+            color: green
+          })];
+        }
+      }), createComponent(Text, {
+        style: rowTitle,
+        children: "Flex End with Flex Grow"
+      }), createComponent(Row, {
+        gap,
+        style: RowStyles,
+        onFocus,
+        get children() {
+          return [createComponent(Block, {
+            color: green
+          }), createComponent(Block, {
+            flexGrow: 1,
+            color: red
+          }), createComponent(Block, {
+            flexGrow: 2,
+            color: darkorange
+          })];
+        }
+      }), createComponent(Text, {
+        style: rowTitle,
+        children: "Space Between with Flex Grow"
+      }), createComponent(Row, {
+        gap,
+        justifyContent: "spaceBetween",
+        style: RowStyles,
+        onFocus,
+        get children() {
+          return [createComponent(Block, {
+            flexGrow: 1,
+            color: red
+          }), createComponent(Block, {
+            flexGrow: 1,
+            flexOrder: -1,
+            color: darkorange
+          }), createComponent(Block, {
+            flexGrow: 2,
+            color: green
+          })];
+        }
+      })];
+    }
+  });
+};
+export {
+  FlexGrow as default
+};
