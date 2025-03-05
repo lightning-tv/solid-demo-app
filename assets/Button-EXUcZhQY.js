e4b044ef05473283aa8f890d96738e8e826d2eeb
Chris Lorenzo <chiefcll@gmail.com>
2025-03-05 09:33:21 -0500

    Updates

---
+++ assets/Button-EXUcZhQY.js
@@ -0,0 +1,64 @@
+import { h as createComponent, m as mergeProps, T as Text, z as combineStyles, Z as styles, V as View } from "./index-CDYBrYjd.js";
+const Button = (props) => {
+  return createComponent(ButtonContainer, mergeProps(props, {
+    get color() {
+      return props.backgroundColor;
+    },
+    get itemSpacing() {
+      return props.contentSpacing;
+    },
+    alignItems: "center",
+    forwardStates: true,
+    get children() {
+      return createComponent(Text, {
+        get color() {
+          return props.textColor;
+        },
+        get contain() {
+          var _a;
+          return (_a = props.contain) != null ? _a : "width";
+        },
+        get textAlign() {
+          return props.textAlign;
+        },
+        get style() {
+          var _a;
+          return combineStyles(
+            styles.Text.tones[(_a = props.tone) != null ? _a : styles.tone],
+            //
+            styles.Text.base
+          );
+        },
+        get children() {
+          return props.children;
+        }
+      });
+    }
+  }));
+};
+const ButtonContainer = (props) => {
+  return createComponent(View, mergeProps(props, {
+    get color() {
+      return props.backgroundColor;
+    },
+    get justifyContent() {
+      return props.justify;
+    },
+    get itemSpacing() {
+      return props.contentSpacing;
+    },
+    get style() {
+      var _a, _b;
+      return combineStyles(
+        props.style,
+        //
+        (_b = styles.Container.tones) == null ? void 0 : _b[(_a = props.tone) != null ? _a : styles.tone]
+      );
+    },
+    forwardStates: true
+  }));
+};
+export {
+  Button as B,
+  ButtonContainer as a
+};
---
+++ assets/ButtonsMaterial-BeRquvKK.js
@@ -0,0 +1,67 @@
+import { x as hexColor, h as createComponent, R as Row, V as View, m as mergeProps, T as Text, M as MaterialButtonText } from "./index-CDYBrYjd.js";
+const MaterialButtonsPage = () => {
+  function onEnter(event, elm) {
+    this.states.toggle("disabled");
+  }
+  const RowStyles = {
+    display: "flex",
+    justifyContent: "flexStart",
+    width: 1500,
+    height: 300,
+    color: hexColor("00000000"),
+    gap: 26,
+    y: 400,
+    x: 100
+  };
+  const MaterialButton = {
+    width: 386,
+    height: 136,
+    color: "0x715cabff",
+    $focus: {
+      color: "0x5a39a2ff"
+    },
+    disabled: {
+      color: "0x291d43ff"
+    }
+  };
+  const RoundedRectangle = ["RoundedRectangle", {
+    radius: 65
+  }];
+  function Button(props) {
+    return createComponent(View, mergeProps(props, {
+      forwardStates: true,
+      style: MaterialButton,
+      shader: RoundedRectangle,
+      get children() {
+        return createComponent(Text, {
+          style: MaterialButtonText,
+          get children() {
+            return props.children;
+          }
+        });
+      }
+    }));
+  }
+  return createComponent(Row, {
+    style: RowStyles,
+    get children() {
+      return [createComponent(Button, {
+        autofocus: true,
+        onEnter,
+        children: "Focused"
+      }), createComponent(Button, {
+        states: {
+          active: true,
+          disabled: false
+        },
+        children: "Normal"
+      }), createComponent(Button, {
+        states: "disabled",
+        children: "Disabled"
+      })];
+    }
+  });
+};
+export {
+  MaterialButtonsPage as default
+};
