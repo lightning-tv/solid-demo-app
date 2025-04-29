1a703550720596237075b0ea757e94ad83c696c2
Chris Lorenzo <chiefcll@gmail.com>
2025-04-29 09:51:03 -0400

    demo using announcer speak directly

--- src/pages/Portal.tsx
+++ src/pages/Portal.tsx
@@ -2,6 +2,7 @@ import { createSignal, createSelector, For, children, createEffect } from "solid
 import { ElementNode, View, Text, assertTruthy } from "@lightningtv/solid";
 import { Column, Row, useFocusStack } from "@lightningtv/solid/primitives";
 import { useNavigate } from "@solidjs/router";
+import { Announcer } from "@lightningtv/solid/primitives";
 import styles from "../styles";
 
 const Portal = (props) => {
@@ -219,6 +220,10 @@ const Portal = (props) => {
       restoreFocus();
     }
   });
+
+  setTimeout(() => {
+    Announcer.speak('Welcome to the examples');
+  }, 1000);
 
   return (
     <>
