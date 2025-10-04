4193155a0b9f00dc8012c005040e4b235570d058
Chris Lorenzo <chiefcll@gmail.com>
2025-10-03 21:16:05 -0400

    Keep alive route (#23)
    * work on keep alive route
    
    * demo off keepAlive
    
    * remove unused import

--- src/api/browsePreload.ts
+++ src/api/browsePreload.ts
@@ -1,10 +1,17 @@
-import { createMemo } from "solid-js";
+import * as s from "solid-js";
 import browseProvider from "./providers/browse";
 
-export function browsePreload({ params }) {
-  return createMemo(() => {
+export function browsePreload(props) {
+  let lastFilter = null;
+  return s.createMemo((p) => {
+    const params = props.params;
+    if (p && (!params.filter || lastFilter === params.filter)) {
+      return p;
+    }
     const provider = browseProvider(params.filter || "all");
     provider(1);
+    lastFilter = params.filter || lastFilter;
+
     return provider;
   });
 }
