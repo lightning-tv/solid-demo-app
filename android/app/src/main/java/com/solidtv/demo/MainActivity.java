da80c011f67c03bae60c7b0aac12a7ac8b44d5a6
Chris Lorenzo <chiefcll@gmail.com>
2026-03-17 16:06:17 -0400

    add build for android support

---
+++ android/app/src/main/java/com/solidtv/demo/MainActivity.java
@@ -0,0 +1,5 @@
+package com.solidtv.demo;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.solidtv.demo',
+  appName: 'solid-demo-app',
+  webDir: 'dist'
+};
+
+export default config;
